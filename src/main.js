0f4cccd40aae2c6b9ea6e8ca3137293f5b142fd0
dasslj <dasslj@163.com>
2024-08-11 21:18:37 +0800

    93

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import { createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import * as echarts from "echarts"
-
-
-
-
 const app = createApp(App)
 
 app.provide("$echarts", echarts)
